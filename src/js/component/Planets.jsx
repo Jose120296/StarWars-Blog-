37f6f3228e05de6a95751a867d3d8ae89ac95b3a
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2024-01-15 15:35:55 +0000

    corregido

--- src/js/component/Planets.jsx
+++ src/js/component/Planets.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Context } from '../store/appContext';
 
+
 export const Planet = ({ name, id }) => {
   const { actions } = useContext(Context);
 

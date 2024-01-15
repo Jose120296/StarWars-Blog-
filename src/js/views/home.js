0e1b6138620577c24944b20e4e5ec47457ea288a
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2024-01-15 14:43:45 +0000

    home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,93 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React, { useEffect, useState } from 'react';
+import '../../styles/home.css';
+import { Character } from '../component/Characters.jsx';
+import { Planet } from '../component/Planets.jsx';
+import { Vehicle } from '../component/Vehicles.jsx';
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+  const [characters, setCharacters] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [vehicles, setVehicles] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      try {
+        const response = await fetch('https://www.swapi.tech/api/people/');
+        const data = await response.json();
+        console.log('Fetched data Characters:', data);
+        setCharacters(data.results);
+      } catch (error) {
+        console.error('Error fetching characters:', error);
+      }
+    };
+
+    fetchCharacters();
+  }, []);
+
+  useEffect(() => {
+    const fetchPlanets = async () => {
+      try {
+        const response = await fetch('https://www.swapi.tech/api/planets/');
+        const data = await response.json();
+        console.log('Fetched data Planets:', data);
+        setPlanets(data.results);
+      } catch (error) {
+        console.error('Error fetching planets:', error);
+      }
+    };
+
+    fetchPlanets();
+  }, []);
+
+  useEffect(() => {
+    const fetchVehicles = async () => {
+      try {
+        const response = await fetch('https://www.swapi.tech/api/vehicles/');
+        const data = await response.json();
+        console.log('Fetched data Vehicles:', data);
+        setVehicles(data.results);
+      } catch (error) {
+        console.error('Error fetching vehicles:', error);
+      }
+    };
+
+    fetchVehicles();
+  }, []);
+
+  return (
+    <div className="characters-container container">
+      <h2 className="container mt-1">Characters</h2>
+      <div className="card-container">
+        {characters.map((characterData) => (
+          <Character
+            id={characterData.uid}
+            key={characterData.uid}
+            name={characterData.name}
+          />
+        ))}
+      </div>
+
+      <h2 className="container mt-4">Planets</h2>
+      <div className="card-container">
+        {planets.map((planetData) => (
+          <Planet
+            id={planetData.uid}
+            key={planetData.uid}
+            name={planetData.name}
+          />
+        ))}
+      </div>
+
+      <h2 className="container mt-4">Vehicles</h2>
+      <div className="card-container">
+        {vehicles.map((vehicleData) => (
+          <Vehicle
+            id={vehicleData.uid}
+            key={vehicleData.uid}
+            name={vehicleData.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
